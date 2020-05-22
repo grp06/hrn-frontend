b0c047090f85045e3d77d74a678c1a59d44a3c8c
George Pickett <gpickett00@gmail.com>
2020-05-22 16:09:39 -0400

    Might be working

--- src/common/UserControl.js
+++ src/common/UserControl.js
@@ -1,29 +1,63 @@
-import React, { useEffect } from 'react'
-
+import React, { useEffect, useState } from 'react'
+import { makeStyles } from '@material-ui/styles'
 import { MainVideo } from '../components'
-
 import { useGameContext } from '../context/useGameContext'
 import endpointUrl from '../utils/endpointUrl'
 
+const width = window.innerWidth
+const height = window.innerHeight
+const useStyles = makeStyles((theme) => ({
+  videoWrapper: {
+    position: 'fixed',
+    marginTop: 200,
+    width: '100%',
+    height: 'calc(100vh - 64px)',
+    top: '64px',
+    background: '#111',
+  },
+  mainVid: {
+    width: '100%',
+    display: 'flex',
+    '& video': {
+      margin: '0 auto',
+      width: '100%',
+      height: '100vh',
+    },
+  },
+  myVideo: {
+    width: '200px',
+    height: '200px',
+    position: 'absolute',
+    top: '15px',
+    right: '15px',
+    '& video': {
+      borderRadius: 15,
+    },
+  },
+}))
+const {
+  connect,
+  createLocalTracks,
+  createLocalVideoTrack,
+  createLocalAudioTrack,
+} = require('twilio-video')
+
 const UserControl = () => {
-  const { currentRound, userId, roundsData, setRoomId, roomId, setToken } = useGameContext()
-  useEffect(() => {
+  const classes = useStyles()
+  const [myRoomId, setMyRoomId] = useState('')
+  const [partner, setPartner] = useState('')
+
+  const { currentRound, userId, roundsData } = useGameContext()
+  const joinRoom = () => {
     if (roundsData && roundsData.rounds && roundsData.rounds.length && currentRound) {
       const myRound = roundsData.rounds.find((round) => {
         const me =
           round.round_number === currentRound &&
           (round.partnerX_id === parseInt(userId, 10) || round.partnerY_id === parseInt(userId, 10))
-
         return me
       })
-      console.log('myRound.id = ', myRound.id)
-      setRoomId(myRound.id)
-    }
-  }, [currentRound, roundsData])
-
-  useEffect(() => {
-    if (roomId) {
-      console.log('roomId = ', roomId)
+      const roomId = myRound.id
+      setMyRoomId(roomId)
       fetch(`${endpointUrl}/api/token`, {
         method: 'POST',
         headers: {
@@ -33,28 +67,107 @@ const UserControl = () => {
       })
         .then((res) => res.json())
         .then(({ token }) => {
-          setToken(token)
-          fetch(`${endpointUrl}/api/rooms/${roomId}`)
-            .then((apiData) => {
-              return apiData.json()
+          connect(token, {
+            name: roomId,
+            audio: false,
+          }).then(function (room) {
+            // Add video after connecting to the Room
+            createLocalVideoTrack().then(function (localTrack) {
+              localTrack.enable()
+
+              room.localParticipant.publishTrack(localTrack)
             })
-            .then((myRoomInfo) => {
-              console.log('myRoomInfo = ', myRoomInfo)
+            room.participants.forEach((remoteParticipant) => {
+              setPartner(remoteParticipant.identity)
+
+              remoteParticipant.tracks.forEach((track) => {
+                console.log('attaching track = ', track)
+                if (track.track) {
+                  const attachedTrack = track.track.attach()
+                  attachedTrack.muted = true
+                  console.log('attachedTrack = ', attachedTrack)
+                  document.getElementById('remote-media-div').appendChild(attachedTrack)
+                }
+              })
+              remoteParticipant.on('trackSubscribed', (track) => {
+                const remoteDiv = document.getElementById('remote-media-div')
+                if (remoteDiv) {
+                  const attachedTrack = track.attach()
+                  attachedTrack.muted = true
+                  console.log('attachedTrack = ', attachedTrack)
+                  remoteDiv.appendChild(attachedTrack)
+                }
+              })
             })
-            .catch((err) => console.log('err = ', err))
+            // When Participants connect to or disconnect from a Room that you're connected to,
+            // you'll be notified via Participant connection events:
+            room.on('participantConnected', function (remoteParticipant) {
+              setPartner(remoteParticipant.identity)
+              console.log(`Participant "${remoteParticipant.identity}" connected`)
+              remoteParticipant.tracks.forEach((track) => {
+                if (track.isSubscribed) {
+                  const remoteDiv = document.getElementById('remote-media-div')
+                  if (remoteDiv) {
+                    const attachedTrack = track.track.attach()
+                    attachedTrack.muted = true
+                    console.log('attachedTrack = ', attachedTrack)
+                    remoteDiv.appendChild(attachedTrack)
+                  }
+                }
+              })
+              remoteParticipant.on('trackSubscribed', (track) => {
+                console.log('on participantConnected trackSubscribed ')
+                const remoteDiv = document.getElementById('remote-media-div')
+                if (remoteDiv) {
+                  const attachedTrack = track.attach()
+                  attachedTrack.muted = true
+                  console.log('attachedTrack = ', attachedTrack)
+                  remoteDiv.appendChild(attachedTrack)
+                }
+              })
+            })
+            // when the remote participant disconnects, remove their stuff?
+            room.on('participantDisconnected', (remoteParticipant) => {
+              const remoteDiv = document.getElementById('remote-media-div')
+              if (remoteDiv) {
+                remoteDiv.innerHTML = ''
+              }
+            })
+            // when you disconnect, stop our track and detach them
+            room.on('disconnected', function (rum, error) {
+              if (error) {
+                console.log('Unexpectedly disconnected:', error)
+              }
+              rum.localParticipant.tracks.forEach(function (track) {
+                track.unpublish()
+              })
+              const remoteDiv = document.getElementById('remote-media-div')
+              if (remoteDiv) {
+                remoteDiv.innerHTML = ''
+              }
+            })
+          })
         })
     }
-  }, [roomId])
-
-  if (currentRound === 0) {
-    return <div>waiting for event to start</div>
   }
-
   return (
-    <>
-      <MainVideo />
-    </>
+    <div>
+      <button onClick={joinRoom}>Join Room</button>
+      <div>my room id ={myRoomId}</div>
+      <div>
+        my partner:
+        {partner}
+      </div>
+      <div className={classes.videoWrapper}>
+        <div id="my-video" className={classes.myVideo} />
+        <div id="remote-media-div" className={classes.mainVid} />
+      </div>
+    </div>
   )
+  // return (
+  //   <>
+  //     <MainVideo />
+  //   </>
+  // )
 }
-
 export default UserControl
