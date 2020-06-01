3ea7268760994935c8ae2abe1a7e765fb6ed1b82
George Pickett <gpickett00@gmail.com>
2020-06-01 12:53:54 -0400

    Remove unnecessary file

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -2,7 +2,6 @@
 export { default as useStartRound } from './useStartRound'
 // FIXME LIKE THIS: https://spin.atomicobject.com/2018/06/25/circular-dependencies-javascript/
 export { default as useCreateRooms } from './useCreateRooms'
-export { default as useRoom } from './useRoom'
 // FIXME LIKE THIS: https://spin.atomicobject.com/2018/06/25/circular-dependencies-javascript/
 export { default as useTwilio } from './useTwilio'
 export { default as CreateModalButton } from './CreateModalButton'
