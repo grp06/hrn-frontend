6034e2ea3ac5c3e700c052ff69adab7652f78f63
Max Solomonyuk <maxsolely@gmail.com>
2020-06-13 16:00:45 -0700

    Return screen for users who joined during an event, conditionally render thumbing if you had a partner, fixed thumbing styles.

--- src/common/waitingRoomScreens/index.js
+++ src/common/waitingRoomScreens/index.js
@@ -1,3 +1,5 @@
 export { default as SittingOut } from './SittingOut'
 export { default as PartnerCameraIssue } from './PartnerCameraIssue'
 export { default as PartnerDisconnected } from './PartnerDisconnected'
+export { default as UserJoinedDuringRound } from './UserJoinedDuringRound'
+export { default as ConnectingToSomeone } from './ConnectingToSomeone'
