dc4e4d2e87022831d1e3552ab9be67b09a6f0ca6
Max Solomonyuk <maxsolely@gmail.com>
2020-06-15 18:22:49 -0700

    Add no thanks button to thumbing. Conditional render thumbing correctly in video router. Made dark buttons hover state black instead of purple.

--- src/components/VideoRouter.js
+++ src/components/VideoRouter.js
@@ -25,7 +25,7 @@ const VideoRouter = ({ myRound }) => {
       case 'not-started':
         return <ConnectingToSomeone />
       case 'in-between-rounds':
-        return <ThumbsUp userId={userId} myRound={myRound} />
+        return hasPartner ? <ThumbsUp userId={userId} myRound={myRound} /> : <ConnectingToSomeone />
       case 'room-in-progress':
         if (lateArrival) {
           console.log('joined late')
