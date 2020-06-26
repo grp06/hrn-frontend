379873d0673b13ef896d4ddd81f70d48155e155e
Max Solomonyuk <maxsolely@gmail.com>
2020-06-25 19:27:39 -0700

    Initial implementation. Come back and add library that copies stuff.

--- src/common/AdminPanel.js
+++ src/common/AdminPanel.js
@@ -3,6 +3,8 @@ import React from 'react'
 import Button from '@material-ui/core/Button'
 import Grid from '@material-ui/core/Grid'
 import Typography from '@material-ui/core/Typography'
+import List from '@material-ui/core/List'
+import ListItem from '@material-ui/core/ListItem'
 import Divider from '@material-ui/core/Divider'
 import { makeStyles } from '@material-ui/styles'
 
@@ -49,10 +51,19 @@ const useStyles = makeStyles((theme) => ({
   descriptionContainer: {
     marginBottom: '25px',
   },
+  eventPromptModal: {},
 }))
 
 const AdminPanel = ({ eventData, timeState }) => {
   const classes = useStyles()
+
+  const {
+    event_users,
+    id: eventId,
+    start_at: eventStartTime,
+    description: eventDescription,
+    status,
+  } = eventData
 
   const editFormModal = TransitionModal({
     modalBody: <EventForm eventData={eventData} />,
@@ -62,13 +73,54 @@ const AdminPanel = ({ eventData, timeState }) => {
     },
   })
 
-  const {
-    event_users,
-    id: eventId,
-    start_at: eventStartTime,
-    description: eventDescription,
-    status,
-  } = eventData
+  const copyEventPromptModal = TransitionModal({
+    modalBody: (
+      <Grid container direction="column" justify="center" alignItems="center">
+        <div className={classes.eventPromptModal} id="eventPrompt">
+          <Typography>Hi 👋, </Typography>
+          <Typography>
+            I'm hosting a speed-networking event with Hi Right Now at {eventStartTime}. The event
+            will have a structure as follows:
+          </Typography>
+          <List dense>
+            <ListItem key={1}>
+              <Typography>
+                1. You will get paired with one other event participant randomly.
+              </Typography>
+            </ListItem>
+            <ListItem key={2}>
+              <Typography>2. You two will video-chat for 5 minutes.</Typography>
+            </ListItem>
+            <ListItem key={3}>
+              <Typography>
+                3. Decide if you want to exchange contact details with your partner.
+              </Typography>
+            </ListItem>
+            <ListItem key={4}>
+              <Typography>4. Rinse and repeat for an hour!</Typography>
+            </ListItem>
+          </List>
+          <Typography>
+            You can sign-up for the event **for free** here (link to event)! Just make sure to come
+            back to that page 5 minutes before the event so you can get settled in before the event.
+            Can't wait to see your smiling face soon 😁
+          </Typography>
+        </div>
+      </Grid>
+    ),
+    button: {
+      buttonText: '✏️ Share Event',
+      buttonColor: 'secondary',
+      buttonSize: 'medium',
+    },
+    onAcceptFunction: () => {
+      const copyPrompt = document.getElementById('eventPrompt').innerText
+      console.log(copyPrompt)
+      // copyPrompt.current.select()
+      document.execCommand('copy')
+    },
+    onAcceptButtonText: 'Copy this prompt!',
+  })
 
   const renderButton = () => {
     let element
@@ -82,7 +134,12 @@ const AdminPanel = ({ eventData, timeState }) => {
         element = <StartEventButton eventId={eventId} status={status} />
         break
       default:
-        element = editFormModal
+        element = (
+          <div>
+            <div>{editFormModal}</div>
+            <div>{copyEventPromptModal}</div>
+          </div>
+        )
         break
     }
     return element
