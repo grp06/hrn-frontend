2183cfce991aa0d0cffe7c1c42dfe7e61d6d67c1
Max Solomonyuk <maxsolely@gmail.com>
2020-07-26 12:40:07 -0400

    Setup react testing library

---
+++ src/__tests__/ConnectingToSomeone.test.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { render, fireEvent } from '@testing-library/react'
+import { ConnectingToSomeone } from '../common/waitingRoomScreens'
+
+test('return connecting to someone text on the screen', () => {
+  const { getByText } = render(<ConnectingToSomeone />)
+  const connectingToSomeoneDiv = getByText('Connecting you to someone awesome!')
+})
