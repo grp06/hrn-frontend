e578fde1b1ac262ef07810f8683bb21096072a85
Max Solomonyuk <maxsolely@gmail.com>
2020-07-26 13:00:39 -0400

    first test complete.

--- src/__tests__/ConnectingToSomeone.test.js
+++ src/__tests__/ConnectingToSomeone.test.js
@@ -5,4 +5,5 @@ import { ConnectingToSomeone } from '../common/waitingRoomScreens'
 test('return connecting to someone text on the screen', () => {
   const { getByText } = render(<ConnectingToSomeone />)
   const connectingToSomeoneDiv = getByText('Connecting you to someone awesome!')
+  expect(connectingToSomeoneDiv).toBeTruthy()
 })
