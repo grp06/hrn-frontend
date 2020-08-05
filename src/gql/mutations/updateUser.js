788f36f76a6066dd5586e5e3046cf86d5632c249
George Pickett <gpickett00@gmail.com>
2020-08-04 21:16:41 -0400

    Make bio not required

--- src/gql/mutations/updateUser.js
+++ src/gql/mutations/updateUser.js
@@ -1,7 +1,7 @@
 import gql from 'graphql-tag'
 
 const updateUser = gql`
-  mutation($id: Int!, $city: String!, $name: String!, $short_bio: String!, $linkedIn_url: String) {
+  mutation($id: Int!, $city: String!, $name: String!, $short_bio: String, $linkedIn_url: String) {
     update_users(
       where: { id: { _eq: $id } }
       _set: { city: $city, name: $name, short_bio: $short_bio, linkedIn_url: $linkedIn_url }
