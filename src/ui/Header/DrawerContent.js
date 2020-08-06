02928a9e77e275b21203d6621d768c45f921a822
Lan Vu <vungoclancfc@gmail.com>
2020-08-06 12:57:05 -0500

    Complete user drawer content

--- src/ui/Header/DrawerContent.js
+++ src/ui/Header/DrawerContent.js
@@ -2,28 +2,15 @@ import React from 'react'
 import Button from '@material-ui/core/Button'
 import Divider from '@material-ui/core/Divider'
 import Grid from '@material-ui/core/Grid'
-import List from '@material-ui/core/List'
-import ListItem from '@material-ui/core/ListItem'
-import ListItemIcon from '@material-ui/core/ListItemIcon'
-import ListItemText from '@material-ui/core/ListItemText'
-import InboxIcon from '@material-ui/icons/MoveToInbox'
-import MailIcon from '@material-ui/icons/Mail'
-import FeatherIcon from 'feather-icons-react'
 import { Link } from 'react-router-dom'
 import { makeStyles } from '@material-ui/styles'
 
 import { UserDrawerContent } from '.'
 import logo from '../../assets/logoWhite.svg'
 
-const drawerWidth = 200
 const useStyles = makeStyles((theme) => ({
   grow: {
     flexGrow: 1,
-  },
-  listItemText: {
-    fontFamily: 'Muli',
-    color: theme.palette.common.ghostWhiteBody,
-    fontSize: '1rem',
   },
   logo: {
     height: '2.85em',
@@ -56,23 +43,6 @@ const DrawerContent = () => {
       </div>
       <Divider />
       <UserDrawerContent />
-
-      <List>
-        {['Profile', 'My Events', 'Connections', 'Settings', 'Host Event', 'Analytics'].map(
-          (text, index) => (
-            <ListItem button key={text}>
-              <ListItemIcon>
-                {index % 2 === 0 ? (
-                  <FeatherIcon icon="user" stroke="#f4f6fa" size="24" />
-                ) : (
-                  <FeatherIcon icon="package" stroke="#f4f6fa" size="24" />
-                )}
-              </ListItemIcon>
-              <ListItemText disableTypography primary={text} className={classes.listItemText} />
-            </ListItem>
-          )
-        )}
-      </List>
       {/* <HostEventControlsMenu event={event} user={user} /> */}
       {/* {eventStatus === 'pre-event' && (
               <MenuItem className={classes.menuItem}>{handleStartEventModal}</MenuItem>
--- src/ui/Header/UserDrawerContent.js
+++ src/ui/Header/UserDrawerContent.js
@@ -1,6 +1,60 @@
 import React from 'react'
+import { useHistory } from 'react-router-dom'
+import List from '@material-ui/core/List'
+import ListItem from '@material-ui/core/ListItem'
+import ListItemIcon from '@material-ui/core/ListItemIcon'
+import ListItemText from '@material-ui/core/ListItemText'
+import FeatherIcon from 'feather-icons-react'
+import { makeStyles } from '@material-ui/styles'
+
+const useStyles = makeStyles((theme) => ({
+  listItemText: {
+    fontFamily: 'Muli',
+    color: theme.palette.common.ghostWhiteBody,
+    fontSize: '1rem',
+    '&:hover': { color: theme.palette.common.sunray },
+  },
+}))
+
 const UserDrawerContent = () => {
-  return <div>Hello there</div>
+  const classes = useStyles()
+  const history = useHistory()
+  const userDrawerRoutes = [
+    {
+      label: 'My Profile',
+      url: '/my-profile',
+      icon: 'user',
+    },
+    {
+      label: 'Connections',
+      url: '/my-connections',
+      icon: 'users',
+    },
+    {
+      label: 'My Events',
+      url: '/events',
+      icon: 'calendar',
+    },
+  ]
+
+  return (
+    <div>
+      <List>
+        {userDrawerRoutes.map((route) => (
+          <ListItem button disableRipple key={route.label} onClick={() => history.push(route.url)}>
+            <ListItemIcon>
+              <FeatherIcon icon={route.icon} stroke="#f4f6fa" size="24" />
+            </ListItemIcon>
+            <ListItemText
+              disableTypography
+              primary={route.label}
+              className={classes.listItemText}
+            ></ListItemText>
+          </ListItem>
+        ))}
+      </List>
+    </div>
+  )
 }
 
 export default UserDrawerContent
