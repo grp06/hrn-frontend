7eca01d4541a4c817d1bffc2f6210c90505252e2
joexylu <joe.luxiyang@gmail.com>
2020-08-26 12:29:35 -0700

    finish chat box in lobby

--- src/pages/Lobby/EventChatBox.js
+++ src/pages/Lobby/EventChatBox.js
@@ -1,18 +1,75 @@
 import React from 'react'
+import Avatar from '@material-ui/core/Avatar'
+import Divider from '@material-ui/core/Divider'
+import Grid from '@material-ui/core/Grid'
+import Typography from '@material-ui/core/Typography'
+import PersonIcon from '@material-ui/icons/Person'
 import { makeStyles } from '@material-ui/styles'
 
 const useStyles = makeStyles((theme) => ({
   container: {
     width: '100%',
     height: '70%',
-    backgroundColor: 'pink',
-    margin: theme.spacing(4),
+    padding: theme.spacing(1),
+    borderRadius: '4px',
+    border: '2px solid #3e4042',
+    boxShadow: '5px 5px 0 #3e4042',
+    backgroundColor: theme.palette.common.greyCard,
+  },
+  lobbyAttendeesContainer: {
+    width: '100%',
+    height: '10%',
+  },
+  totalAttendeesContainer: {
+    width: 'auto',
+  },
+  chatBox: {
+    height: '88%',
   },
 }))
 
 const EventChatBox = () => {
   const classes = useStyles()
-  return <div className={classes.container}>Event Chat Box</div>
+  return (
+    <Grid container direction="column" justify="space-around" className={classes.container}>
+      <Grid
+        container
+        wrap="nowrap"
+        alignItems="center"
+        justify="space-around"
+        className={classes.lobbyAttendeesContainer}
+      >
+        <Avatar>
+          <PersonIcon />
+        </Avatar>
+        <Grid container direction="column" className={classes.totalAttendeesContainer}>
+          <Grid container>
+            <Typography variant="body1">26</Typography>
+            <Typography variant="body1">Atendees in Lobby</Typography>
+          </Grid>
+          <Grid container>
+            <Typography variant="body1">0</Typography>
+            <Typography variant="body1">Atendees in Chatting</Typography>
+          </Grid>
+        </Grid>
+      </Grid>
+      <Divider />
+      <Grid
+        container
+        direction="column"
+        justify="center"
+        alignItems="center"
+        className={classes.chatBox}
+      >
+        <Typography variant="h6">
+          Chat Coming Soon!{' '}
+          <span role="img" aria-label="sunGlassSmileingFace">
+            😎
+          </span>
+        </Typography>
+      </Grid>
+    </Grid>
+  )
 }
 
 export default EventChatBox
