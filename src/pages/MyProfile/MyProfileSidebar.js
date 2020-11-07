b4581ef9c7bead4114252027abd7edbee7cfdcf4
Max Solomonyuk <maxsolely@gmail.com>
2020-11-06 19:00:02 -0600

    Finalize add profile picture.

--- src/pages/MyProfile/MyProfileSidebar.js
+++ src/pages/MyProfile/MyProfileSidebar.js
@@ -9,6 +9,7 @@ import LinkedInIcon from '@material-ui/icons/LinkedIn'
 import { makeStyles } from '@material-ui/styles'
 
 import { motion } from 'framer-motion'
+import { DropzoneArea } from 'material-ui-dropzone'
 
 import { FloatCardNarrow } from '../../common'
 import { useAppContext } from '../../context'
@@ -24,6 +25,40 @@ const createStyles = makeStyles((theme) => ({
       height: '100%',
     },
   },
+  avatarContainer: {
+    width: '125px',
+    height: '125px',
+  },
+  buttonContainer: {
+    width: '50%',
+    [theme.breakpoints.down('md')]: {
+      width: '50%',
+    },
+  },
+  cancelButton: {
+    margin: theme.spacing(1.5, 0),
+    backgroundColor: theme.palette.common.greyButton,
+    color: theme.palette.common.ghostWhite,
+    '&:hover': {
+      backgroundColor: theme.palette.common.greyButtonHover,
+    },
+  },
+  editProfileButton: {
+    margin: theme.spacing(2, 0),
+  },
+  fileForm: {
+    position: 'absolute',
+    height: '100%',
+    width: '100%',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  linkedInIcon: {
+    marginTop: theme.spacing(1),
+    fontSize: '32px',
+    color: '#3176b0',
+  },
   overlay: {
     position: 'absolute',
     top: 0,
@@ -33,58 +68,15 @@ const createStyles = makeStyles((theme) => ({
     background: 'rgba(0, 0, 0, .7)',
     color: '#fff',
     opacity: 0,
-    // transition: '.2s',
     display: 'flex',
     justifyContent: 'center',
     alignItems: 'center',
     cursor: 'pointer',
-  },
-  wrap: {
-    position: 'relative',
-    width: '125px',
-    height: '125px',
-    marginBottom: theme.spacing(2),
-    cursor: 'pointer',
-  },
-
-  avatarContainer: {
-    width: '125px',
-    height: '125px',
-  },
-
-  buttonContainer: {
-    width: '50%',
-    [theme.breakpoints.down('md')]: {
-      width: '50%',
-    },
-  },
-  editProfileButton: {
-    margin: theme.spacing(2, 0),
-  },
-  linkedInIcon: {
-    marginTop: theme.spacing(1),
-    fontSize: '32px',
-    color: '#3176b0',
   },
   shortBio: {
     marginTop: theme.spacing(3),
     width: '75%',
     textAlign: 'center',
-  },
-  uploadPhotoTarget: {
-    width: '100%',
-    height: '100%',
-    opacity: 0,
-    zIndex: 9999,
-    cursor: 'pointer',
-  },
-  fileForm: {
-    position: 'absolute',
-    height: '100%',
-    width: '100%',
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
   },
   uploadImageText: {
     position: 'absolute',
@@ -95,6 +87,13 @@ const createStyles = makeStyles((theme) => ({
     alignItems: 'center',
     cursor: 'pointer',
     textAlign: 'center',
+  },
+  wrap: {
+    position: 'relative',
+    width: '125px',
+    height: '125px',
+    marginBottom: theme.spacing(2),
+    cursor: 'pointer',
   },
 }))
 
@@ -112,94 +111,120 @@ const MyProfileSidebar = ({ user, databaseTags }) => {
     linkedIn_url,
     profile_pic_url,
   } = user
-  const [showEditSidebarForm, setShowEditSidebarForm] = useState(false)
+  // my-profile, edit-picture, edit-profile
+  const [contentState, setContentState] = useState('my-profile')
 
   const eventIdInLS = localStorage.getItem('eventId')
 
-  const submitFile = async (e) => {
-    setAppLoading(true)
-    console.log('submit form')
-    e.preventDefault()
-    const file = e.target.files
-    try {
-      if (!file) {
-        throw new Error('Select a file first!')
+  const submitProfilePicture = async (file) => {
+    if (file.length) {
+      setAppLoading(true)
+      try {
+        if (!file) {
+          throw new Error('Select a file first!')
+        }
+        const formData = new FormData()
+        formData.append('file', file[0])
+        formData.append('userId', userId)
+        await fetch(`${process.env.REACT_APP_API_URL}/test-upload`, {
+          method: 'POST',
+          headers: {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Credentials': true,
+          },
+          body: formData,
+        })
+        window.location.reload()
+        // handle success
+      } catch (error) {
+        window.location.reload()
+        console.log('submitFile -> error', error)
+        // handle error
       }
-      const formData = new FormData()
-      formData.append('file', file[0])
-      formData.append('userId', userId)
-      const response = await fetch(`${process.env.REACT_APP_API_URL}/test-upload`, {
-        method: 'POST',
-        headers: {
-          'Access-Control-Allow-Origin': '*',
-          'Access-Control-Allow-Credentials': true,
-        },
-        body: formData,
-      }).then((res) => res.json())
-      console.log('submitFile -> response', response.url)
-
-      window.location.reload()
-
-      // handle success
-    } catch (error) {
-      window.location.reload()
-      console.log('submitFile -> error', error)
-      // handle error
     }
   }
 
   useEffect(() => {
     if (usersTags.length === 0) {
-      setShowEditSidebarForm(true)
+      setContentState('edit-profile')
     }
   }, [])
 
-  const renderSidebarContent = () => {
-    return !showEditSidebarForm ? (
-      <Grid container direction="column" alignItems="center" justify="center">
-        <Typography variant="h3">{name}</Typography>
-        <Typography variant="subtitle1">{city}</Typography>
-        {linkedIn_url && (
-          <Button href={linkedIn_url} target="_blank" rel="noopener noreferrer">
-            <LinkedInIcon className={classes.linkedInIcon} />
-          </Button>
-        )}
-        <Typography variant="body1" className={classes.shortBio}>
-          {short_bio}
-        </Typography>
-        {/* <SidebarAchievements /> */}
-        <SidebarTags userId={userId} usersTags={usersTags} databaseTags={databaseTags.tags} />
-        <Grid
-          container
-          justify="space-around"
-          alignItems="center"
-          className={classes.buttonContainer}
-        >
-          <Button
-            variant="contained"
-            color="primary"
-            className={classes.editProfileButton}
-            onClick={() => setShowEditSidebarForm(true)}
-          >
-            Edit Profile
-          </Button>
-          <Button
-            variant="contained"
-            color="secondary"
-            disabled={!eventIdInLS}
-            className={classes.editProfileButton}
-            onClick={() => history.push(`/events/${eventIdInLS}`)}
-          >
-            Back to Event
-          </Button>
-        </Grid>
-      </Grid>
-    ) : (
-      <EditProfileSidebarForm
-        databaseTags={databaseTags}
-        onClose={() => setShowEditSidebarForm(false)}
-      />
-    )
+  const renderContent = () => {
+    switch (contentState) {
+      case 'edit-profile':
+        return (
+          <EditProfileSidebarForm
+            databaseTags={databaseTags}
+            onClose={() => setContentState('my-profile')}
+          />
+        )
+      case 'edit-picture':
+        return (
+          <>
+            <DropzoneArea
+              alertSnackbarProps={{
+                anchorOrigin: { vertical: 'top', horizontal: 'center' },
+              }}
+              acceptedFiles={['image/*']}
+              dropzoneText={'Drag and drop your favorite selfie 🤳. Or click to choose'}
+              filesLimit={1}
+              maxFileSize={500000}
+              onChange={(file) => submitProfilePicture(file)}
+            />
+            <Button
+              variant="outlined"
+              className={classes.cancelButton}
+              onClick={() => setContentState('my-profile')}
+            >
+              Cancel
+            </Button>
+          </>
+        )
+
+      case 'my-profile':
+      default:
+        return (
+          <Grid container direction="column" alignItems="center" justify="center">
+            <Typography variant="h3">{name}</Typography>
+            <Typography variant="subtitle1">{city}</Typography>
+            {linkedIn_url && (
+              <Button href={linkedIn_url} target="_blank" rel="noopener noreferrer">
+                <LinkedInIcon className={classes.linkedInIcon} />
+              </Button>
+            )}
+            <Typography variant="body1" className={classes.shortBio}>
+              {short_bio}
+            </Typography>
+            {/* <SidebarAchievements /> */}
+            <SidebarTags userId={userId} usersTags={usersTags} databaseTags={databaseTags.tags} />
+            <Grid
+              container
+              justify="space-around"
+              alignItems="center"
+              className={classes.buttonContainer}
+            >
+              <Button
+                variant="contained"
+                color="primary"
+                className={classes.editProfileButton}
+                onClick={() => setContentState('edit-profile')}
+              >
+                Edit Profile
+              </Button>
+              <Button
+                variant="contained"
+                color="secondary"
+                disabled={!eventIdInLS}
+                className={classes.editProfileButton}
+                onClick={() => history.push(`/events/${eventIdInLS}`)}
+              >
+                Back to Event
+              </Button>
+            </Grid>
+          </Grid>
+        )
+    }
   }
   return (
     <FloatCardNarrow>
@@ -208,22 +233,16 @@ const MyProfileSidebar = ({ user, databaseTags }) => {
           <Avatar className={classes.avatarContainer}>
             <img alt="company-logo" className={classes.avatar} src={profile_pic_url || logo} />
 
-            <motion.div className={classes.overlay} whileHover={{ opacity: 1 }}>
-              <form onSubmit={submitFile} className={classes.fileForm}>
-                <input
-                  type="file"
-                  onChange={async (event) => {
-                    submitFile(event)
-                  }}
-                  className={classes.uploadPhotoTarget}
-                />
-
-                <span className={classes.uploadImageText}>edit profile picture</span>
-              </form>
+            <motion.div
+              className={classes.overlay}
+              whileHover={{ opacity: 1 }}
+              onClick={() => setContentState('edit-picture')}
+            >
+              <span className={classes.uploadImageText}>edit profile picture</span>
             </motion.div>
           </Avatar>
         </div>
-        {renderSidebarContent()}
+        {renderContent()}
       </Grid>
     </FloatCardNarrow>
   )
