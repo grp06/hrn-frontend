02376504c02aac58f4a02cdc9eff14c11f02da51
Max Solomonyuk <maxsolely@gmail.com>
2020-11-14 18:33:52 -0600

    Add lottie background to pricing hero.

--- src/pages/Pricing/PricingHero.js
+++ src/pages/Pricing/PricingHero.js
@@ -1,41 +1,75 @@
 import React from 'react'
 import Button from '@material-ui/core/Button'
-import Divider from '@material-ui/core/Divider'
 import Grid from '@material-ui/core/Grid'
 import Typography from '@material-ui/core/Typography'
 import { makeStyles } from '@material-ui/core/styles'
+import Lottie from 'react-lottie'
+import * as confettiAnimation from '../../assets/confettiFalling.json'
 
 const useStyles = makeStyles((theme) => ({
+  backgroundLottie: {
+    position: 'absolute',
+    width: '100%',
+    height: '100%',
+    zIndex: -99,
+  },
+  becomeAHostButton: {
+    margin: theme.spacing(3, 0, 1, 0),
+  },
   cardContainer: {
     position: 'relative',
-    backgroundColor: '#ffe7ec',
+    // backgroundColor: '#ffe7ec',
     // backgroundColor: '#fdeacd',
     // border: '1px solid #FF99AD',
     // boxShadow: '4px 4px 0 #FF99AD',
     // backgroundColor: '#d9c8f3',
-    border: '2px solid #8C57DB',
-    boxShadow: '4px 4px 0 #8C57DB',
+    border: '2px solid #FF99AD',
+    boxShadow: '4px 4px 0 #FF99AD',
     borderRadius: '4px',
+  },
+  heroContent: {
     padding: theme.spacing(3, 5),
   },
 }))
 
 const PricingHero = () => {
   const classes = useStyles()
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: confettiAnimation.default,
+    rendererSettings: {
+      preserveAspectRatio: 'xMidYMid slice',
+    },
+  }
+
   return (
     <Grid container direction="column" className={classes.cardContainer}>
-      <Typography variant="h2" style={{ color: 'black' }}>
-        Host your own events{' '}
-      </Typography>
-      <Typography variant="body1" style={{ color: 'black' }}>
-        Create events for your community or tap into the Hi Right Now community
-      </Typography>
-      <Typography variant="body1" style={{ color: 'black' }}>
-        Try our Basic Plan for free!y
-      </Typography>
-      <Button variant="contained" size="large" color="primary">
-        Join Basic
-      </Button>
+      <div className={classes.backgroundLottie}>
+        <Lottie options={defaultOptions} height="100%" width="100%" />
+      </div>
+      <Grid className={classes.heroContent}>
+        <Typography variant="h1" style={{ color: 'black', marginBottom: '8px' }}>
+          Host 2 events on us{' '}
+          <span role="img" aria-label="wink face">
+            😉
+          </span>
+        </Typography>
+        <Typography variant="h4" style={{ color: 'black' }}>
+          Create an event for your community and get people involved today.
+        </Typography>
+        <Typography variant="h4" style={{ color: 'black' }}>
+          No payments, no sign up, no hassle!
+        </Typography>
+        <Button
+          variant="contained"
+          size="large"
+          color="primary"
+          className={classes.becomeAHostButton}
+        >
+          Become a Host
+        </Button>
+      </Grid>
     </Grid>
   )
 }
