6e2ea7fd36b0f46b5b300194125be522e5d25089
Max Solomonyuk <maxsolely@gmail.com>
2020-12-04 20:54:26 -0600

    Figure out react router shit.

--- src/context/UserProvider.js
+++ src/context/UserProvider.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react'
 import { useImmer } from 'use-immer'
 import { useQuery } from '@apollo/react-hooks'
-import { useHistory } from 'react-router-dom'
+import { useHistory, useLocation } from 'react-router-dom'
 import { useAppContext } from '.'
 import { findUserById } from '../gql/queries'
 
@@ -17,8 +17,9 @@ const UserProvider = ({ children }) => {
   const [state, dispatch] = useImmer({ ...defaultState })
   const { setAppLoading } = useAppContext()
   const history = useHistory()
+  const location = useLocation()
   const { userId } = state.user
-  const { pathname } = history.location
+  const { pathname } = location
 
   const specificEventPageRegex = /\/events\/\d+[\/]?$/
   const eventsPageRegex = /\/events[\/]?$/
@@ -33,6 +34,15 @@ const UserProvider = ({ children }) => {
     pathname.includes('video-room') ||
       pathname.includes('lobby') ||
       pathname.includes('group-video-chat')
+  )
+  const isUserOnAuth = Boolean(
+    pathname === '/' ||
+      pathname.includes('sign-up') ||
+      pathname.includes('forgot-password') ||
+      pathname.includes('set-new-password') ||
+      pathname.includes('onboarding') ||
+      pathname.includes('host-onboarding') ||
+      pathname.includes('checkout-success')
   )
 
   const { data: userData } = useQuery(findUserById, {
@@ -54,22 +64,13 @@ const UserProvider = ({ children }) => {
   }, [userData, userId])
 
   useEffect(() => {
-    console.log('pathname ->', pathname)
-    const isUserOnAuth = Boolean(
-      pathname === '/' ||
-        pathname.includes('sign-up') ||
-        pathname.includes('forgot-password') ||
-        pathname.includes('set-new-password') ||
-        pathname.includes('onboarding') ||
-        pathname.includes('host-onboarding') ||
-        pathname.includes('checkout-success')
-    )
-
-    console.log(isUserOnAuth)
-    dispatch((draft) => {
-      draft.userOnAuthRoute = isUserOnAuth
-    })
-  }, [pathname])
+    if (location) {
+      dispatch((draft) => {
+        draft.userOnAuthRoute = isUserOnAuth
+        draft.userOnEventsPage = userInEvent
+      })
+    }
+  }, [location])
 
   // once we get on the app check to see if theres a userID in local storage
   // if there is then we want to set user.userId so that findByUserId query can be called
