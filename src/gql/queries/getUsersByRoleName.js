97dbc40a764841db9ec15fda48b8c26021e3ed21
Max Solomonyuk <maxsolely@gmail.com>
2020-12-14 15:44:28 -0500

    Add paid host dashboard.

--- src/gql/queries/getUsersByRoleName.js
+++ src/gql/queries/getUsersByRoleName.js
@@ -1,8 +1,8 @@
 import gql from 'graphql-tag'
 
 const getUsersByRoleName = gql`
-  query getUsersByRoleName($role: String!) {
-    users(where: { role: { _eq: $role } }) {
+  query getUsersByRoleName($role: String!, $now: timestamptz) {
+    users(where: { _and: [{ role: { _eq: $role } }, { sub_period_end: { _gt: $now } }] }) {
       city
       email
       name
