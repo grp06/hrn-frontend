e89cb69e8577a205601228ea8b62129438648e11
Max Solomonyuk <maxsolely@gmail.com>
2021-01-19 22:24:39 +0800

    Successfully sign up as a fan and get token back. Need to serialize the phone number and role on the backend and send back to front end, but other than that its working.

---
+++ src/helpers/signupByRole.js
@@ -0,0 +1,22 @@
+const signupByRole = async ({ role, userInfo }) => {
+  const { cashApp, email, name, password, phone_number, venmo } = userInfo
+  return fetch(`${process.env.REACT_APP_API_URL}/api/signup`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true,
+    },
+    body: JSON.stringify({
+      cash_app: cashApp,
+      email,
+      name,
+      password,
+      phone_number,
+      role,
+      venmo,
+    }),
+  }).then((res) => res.json())
+}
+
+export default signupByRole
