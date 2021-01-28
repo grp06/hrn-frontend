dcf687e17457dbe434d6e5c78b77b4bbb1200ee5
Max Solomonyuk <maxsolely@gmail.com>
2021-01-28 13:51:00 +0800

    Fix host event click from webflow bug for hosts and users but not for anonymous users.

--- src/pages/Auth/SignUp.js
+++ src/pages/Auth/SignUp.js
@@ -1,10 +1,16 @@
 import React, { useEffect, useState } from 'react'
 import { useLocation, Redirect } from 'react-router-dom'
 import { SignUpForm } from '.'
+import { Loading } from '../../common'
+import { useAppContext, useUserContext } from '../../context'
 import { getSubscriptionCheckoutObject } from '../Subscription'
 
 const SignUp = () => {
   const location = useLocation()
+  const { appLoading } = useAppContext()
+  const {
+    user: { role: usersRole },
+  } = useUserContext()
   const searchParams = new URLSearchParams(location.search)
   const planType = searchParams.get('planType')
   const billingPeriod = searchParams.get('billingPeriod')
@@ -20,11 +26,19 @@ const SignUp = () => {
     setCheckedSCOInLS(true)
   }, [billingPeriod, planType])
 
+  if (appLoading) {
+    return <Loading />
+  }
+
   // check to see if a user is already logged in, if so redirect
   if (checkedSCOInLS && localStorage.getItem('userId')) {
     const subCheckoutObjectFromLS = localStorage.getItem('subscriptionCheckoutObject')
-    if (subCheckoutObjectFromLS) {
+    if (subCheckoutObjectFromLS && (!usersRole || usersRole === 'user')) {
+      console.log('im nothing boiii')
       return <Redirect to={{ pathname: '/checkout', state: JSON.parse(subCheckoutObjectFromLS) }} />
+    } else if (subCheckoutObjectFromLS && usersRole.includes('host')) {
+      console.log('im a host')
+      return <Redirect to={{ pathname: '/create-event' }} />
     }
     return <Redirect to="/events" />
   }
