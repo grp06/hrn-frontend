2da86c0642bf62d6938c89f094824d7cec666643
Max Solomonyuk <maxsolely@gmail.com>
2021-03-02 09:58:47 +0800

    Finish EventComplete page.

--- src/pages/EventComplete/BecomeAHostCard.js
+++ src/pages/EventComplete/BecomeAHostCard.js
@@ -1,42 +1,12 @@
 import React from 'react'
 import Lottie from 'react-lottie'
 import { useHistory } from 'react-router-dom'
-import Button from '@material-ui/core/Button'
-import Grid from '@material-ui/core/Grid'
-import Typography from '@material-ui/core/Typography'
-import { makeStyles } from '@material-ui/core/styles'
+import { Button, Grid, Typography } from '@material-ui/core'
+import { useEventCompleteStyles } from '.'
 import * as confettiAnimation from '../../assets/confettiFalling.json'
 
-const useStyles = makeStyles((theme) => ({
-  backgroundLottie: {
-    position: 'absolute',
-    width: '100%',
-    height: '100%',
-    zIndex: -99,
-  },
-  becomeAHostButton: {
-    margin: theme.spacing(3, 0, 1, 0),
-    width: '100%',
-  },
-  cardContainer: {
-    position: 'relative',
-    // backgroundColor: '#ffe7ec',
-    // backgroundColor: '#fdeacd',
-    // border: '1px solid #FF99AD',
-    // boxShadow: '4px 4px 0 #FF99AD',
-    // backgroundColor: '#d9c8f3',
-    border: '2px solid #FF99AD',
-    boxShadow: '4px 4px 0 #FF99AD',
-    borderRadius: '4px',
-    marginBottom: theme.spacing(3),
-  },
-  heroContent: {
-    padding: theme.spacing(3, 5),
-  },
-}))
-
 const BecomeAHostCard = () => {
-  const classes = useStyles()
+  const classes = useEventCompleteStyles()
   const history = useHistory()
   const defaultOptions = {
     loop: true,
@@ -48,11 +18,11 @@ const BecomeAHostCard = () => {
   }
 
   return (
-    <Grid container direction="column" className={classes.cardContainer}>
+    <Grid container direction="column" className={classes.becomeAHostCardContainer}>
       <div className={classes.backgroundLottie}>
         <Lottie options={defaultOptions} height="100%" width="100%" />
       </div>
-      <Grid className={classes.heroContent}>
+      <Grid className={classes.becomeAHostHeroContent}>
         <Typography variant="h2" style={{ color: 'black', marginBottom: '8px' }}>
           Host an event on us{' '}
           <span role="img" aria-label="wink face">
--- src/pages/EventComplete/index.js
+++ src/pages/EventComplete/index.js
@@ -1,3 +1,4 @@
 export { default as BecomeAHostCard } from './BecomeAHostCard'
 export { default as EventComplete } from './EventComplete'
 export { default as MyEventConnectionsList } from './MyEventConnectionsList'
+export { default as useEventCompleteStyles } from './styles'
