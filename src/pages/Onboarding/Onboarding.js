80eef03f7e3929a97f193d3a3b99421c6a1e9a76
George Pickett <gpickett00@gmail.com>
2021-04-27 18:29:39 +0800

    add plan name free for customer.io

--- src/pages/Onboarding/Onboarding.js
+++ src/pages/Onboarding/Onboarding.js
@@ -87,6 +87,7 @@ const Onboarding = () => {
         email: usersEmail,
         role,
         city: values.city,
+        plan_name: 'free',
       })
       window.analytics.track('Onboarding complete', {
         registeredViaEventId: eventIdInLocalStorage || 'none',
