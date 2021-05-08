2049aa622ac37c4bc9659089b86be67e0b6f8195
George Pickett <gpickett00@gmail.com>
2021-05-08 21:13:48 +0800

    Add query for getTwilioToken

--- src/helpers/getToken.js
+++ src/helpers/getToken.js
@@ -1,5 +1,6 @@
 const getToken = async (uniqueName, userId) => {
-  return fetch(`${process.env.REACT_APP_API_URL}/api/token`, {
+  // wont be used anymore
+  return fetch(`${process.env.REACT_APP_API_URL}/api/twilio/get-token`, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
